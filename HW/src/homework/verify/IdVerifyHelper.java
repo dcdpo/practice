bfa199a53ee45472af7ff68289d7f8076498a69d
David <sodagreenapay@gmail.com>
2022-12-21 09:55:34 +0800

    修改版面1

--- HW/src/homework/verify/IdVerifyHelper.java
+++ HW/src/homework/verify/IdVerifyHelper.java
@@ -6,14 +6,17 @@ import java.util.List;
 
 public class IdVerifyHelper {
 
-    //讀取檔案
+    /**
+     *讀取檔案
+     * @param filePath
+     * @return myList
+     */
     public static List readFile(String filePath) {
-        ArrayList myList = null;
+        ArrayList myList = new ArrayList();
 
         try {
             BufferedReader inFile = new BufferedReader(new FileReader(filePath));//用BufferedReader去做檔案的讀取
             String lineTxt = null;
-            myList = new ArrayList();
 
             while ((lineTxt = inFile.readLine()) != null) {
                 //inFile.readLine()是指讀取txt檔的每一行資料,把讀到的資料存到lineTxt
@@ -26,21 +29,23 @@ public class IdVerifyHelper {
         return myList;
     }
 
-    //身分驗證器
+    /**
+     *計算並且驗證
+     * @param idStr
+     */
     public static void verify(String idStr) {
 
         try {
             int data[] = {10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 33};
             int val = data[idStr.charAt(0) - 'A']; //利用charAt取得idStr的第一個字母，與A相減取得相應的index
-            int p = (val / 10) + (9 * (val % 10)); //p為放加權後相加結果的變數，先計算第一個字母的值
-            int j = 1;
+            int total = (val / 10) + (9 * (val % 10)); //p為放加權後相加結果的變數，先計算第一個字母的值
+            int count = 1;
             // 用一個for迴圈去計算字母之後8個位數的加權
             for (int i = 8; i >= 1; i--) {
-                System.out.println("j =" + j);
-                p += i * Integer.parseInt(idStr.substring(j, j + 1));
-                j++;
+                total += i * Integer.parseInt(idStr.substring(count, count + 1));
+                count++;
             }
-            int checkCode = (10 - (p % 10)) % 10;//取得驗證碼
+            int checkCode = (10 - (total % 10)) % 10;//取得驗證碼
 
             if (String.valueOf(idStr.charAt(9)).equals(String.valueOf(checkCode))) {
                 System.out.println("====您輸入的身分證字號" + idStr + "====");
@@ -51,18 +56,16 @@ public class IdVerifyHelper {
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("====您輸入的身分證字號" + idStr + "====");
-            System.out.println("====輸入不正確，請再試一次====");
+            System.out.println("====驗證失敗====");
         }
     }
 
     public static void main(String[] args) {
-        String filePath = "C:\\Users\\88698\\Downloads\\idList.txt";
+        String filePath = "./HW/src/homework/verify/idList.txt";
         List list = readFile(filePath);
-        String idStr = null;
 
-        for (int i = 0; i < list.size(); i++) {
-            idStr = (String) list.get(i);
-            verify(idStr);
+        for (Object idStr: list) {
+            verify(String.valueOf(idStr));
         }
     }
 }
