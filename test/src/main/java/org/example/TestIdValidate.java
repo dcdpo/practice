300372d273b0d2768a796d9a1a831a612e153d48
David <sodagreenapay@gmail.com>
2022-12-22 10:35:50 +0800

    1223-更新hw2格式

--- test/src/main/java/org/example/TestIdValidate.java
+++ test/src/main/java/org/example/TestIdValidate.java
@@ -1,10 +1,12 @@
-package homework.verify;
+package org.example;
 
-public class TestIdVelidate {
 
+import homework.verify.IdVerifyHelper;
+
+public class TestIdValidate {
     public static void main(String[] args) {
         IdVerifyHelper idVerifyHelper = new IdVerifyHelper("");
         System.out.println(idVerifyHelper.validate("C:\\practice\\HW\\src\\homework\\verify\\idList2.txt"));
-    }
 
+    }
 }
