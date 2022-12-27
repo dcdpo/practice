a83ec949fb8b7ec80a499e2629466ee4487e9f58
David <sodagreenapay@gmail.com>
2022-12-27 17:00:18 +0800

    1227 創建類別

---
+++ demo/src/main/java/com/example/demo/Service/MemberService.java
@@ -0,0 +1,27 @@
+package com.example.demo.Service;
+
+import com.example.demo.Controller.Member;
+
+import java.util.List;
+
+public class MemberService {
+    public List<Member>getAllMember(){
+        return null;
+    }
+
+    public List<Member> getAllTeacher(){
+        return null;
+    }
+
+    public List<Member> getAllStudent(){
+        return  null;
+    }
+
+    public Member getTeacher(String teacherId){
+        return null;
+    }
+
+    public Member getStudent(String studentId){
+        return null;
+    }
+}
