c5dcdc76e1cb386ee7a6f19d6ce386ead083506e
David <sodagreenapay@gmail.com>
2023-01-04 17:09:37 +0800

    1/4 修版面

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Member;
-import com.example.demo.entity.Teacher;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class MemberController {
