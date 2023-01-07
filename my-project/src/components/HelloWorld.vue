fdf28afaf9f9b932ad99cb4f12c9e0c3f6f3d7be
David <sodagreenapay@gmail.com>
2023-01-07 17:09:21 +0800

    1/7 在對的地方，建立vue專案、輸入資料

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,166 @@
+<template>
+  <h1>查詢功能</h1>
+  <div class="input-item">
+    <label class="form-label" for="input01">
+      學號
+    </label>
+    <input
+        id="input01"
+        class="form-control"
+        type="text"
+        name="text"
+        required
+    />
+    <br>
+    <label class="form-label" for="input01">
+      職位
+    </label>
+    <select
+        class="form-select"
+        aria-label="select"
+    >
+      <option selected>
+        無
+      </option>
+      <option value="teacher">
+        老師
+      </option>
+      <option value="student">
+        學生
+      </option>
+    </select>
+  </div>
+  <br>
+  <button
+      type="button"
+      class="btn btn-primary"
+  >
+    清除
+  </button>
+  <button
+      type="button"
+      class="btn btn-primary"
+  >
+    查詢
+  </button>
+  <br><br><br><br><br>
+  <table style="margin-left: auto;margin-right: auto;">
+    <thead>
+    <tr>
+      <th class="white-space-nowrap">
+        學號
+      </th>
+      <th class="white-space-nowrap">
+        姓名
+      </th>
+      <th class="white-space-nowrap">
+        性別
+      </th>
+      <th class="white-space-nowrap">
+        科目
+      </th>
+      <th class="white-space-nowrap">
+        職位
+      </th>
+      <th class="white-space-nowrap">
+        班級
+      </th>
+      <th class="white-space-nowrap">
+        入學年度
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>
+        1
+      </td>
+      <td>
+        jacky
+      </td>
+      <td>
+        男
+      </td>
+      <td>
+        無
+      </td>
+      <td>
+        無
+      </td>
+      <td>
+        301
+      </td>
+      <td>
+        201910
+      </td>
+    </tr>
+    </tbody>
+  </table>
+
+</template>
+
+<script>
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     members: [
+//       {
+//         id: 1,
+//         name: 'Billy',
+//         gender: 'male',
+//         subject: '數學',
+//         jobTitle: '教務主任'
+//       },
+//       {
+//         id: 2,
+//         name: 'Heidi',
+//         gender: 'female',
+//         subject: '英文',
+//         jobTitle: '教師'
+//       },
+//       {
+//         id: 3,
+//         name: 'Jacky',
+//         gender: 'male',
+//         class: 301,
+//         admissionYearMonth: 201910
+//       },
+//       {
+//         id: 4,
+//         name: 'Lawrence',
+//         gender: 'male',
+//         class: 801,
+//         admissionYearMonth: 201812
+//       }
+//     ],
+//     search:'',
+//   }
+// })
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
