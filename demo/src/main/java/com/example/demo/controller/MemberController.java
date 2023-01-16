2039fa5313302448612d144184a09507b7b15e08
David <sodagreenapay@gmail.com>
2023-01-16 17:25:54 +0800

    1/16 改跨域問題

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -111,6 +111,7 @@ public class MemberController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @CrossOrigin
     public ResponseEntity<?> deleteMember(@PathVariable Integer id) {
 
         try {
